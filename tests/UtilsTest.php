ae91b7176a78754bd9bf5d7a5e85b187c9bc4fe6
Ilya Shirokov <asd.mat@mail.ru>
2023-04-18 11:10:59 +0300

    Add/Update file

---
+++ tests/UtilsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hexlet\Phpunit\Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Hexlet\Phpunit\Utils\reverseString;
+
+Class UtilsTest extends TestCase
+{
+    public function testReverse(): void
+    {
+        $this->assertEquals('', reverseString(''));
+        $this->assertEquals('olleh', reverseString('hello'));
+    }
+}
+
+?>
